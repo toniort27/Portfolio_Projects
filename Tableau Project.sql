9ebdfe76a9cf0646feb173830b346f11e8887577
Antonio Ripoll <80843791+toniort27@users.noreply.github.com>
2021-06-16 21:55:07 -0500

    Add files via upload

---
+++ Tableau Project.sql
@@ -0,0 +1,71 @@
+/*
+Queries I used for my Tableau Skills Project
+*/
+
+
+
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where location = 'Colombia'
+where continent is not null 
+--Group By date
+order by 1,2
+
+-- 2. 
+
+-- In an effort to stay consistent, I removed these ones since they are not included in the earlier query
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location = 'Colombia'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location = 'Colombia'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location = 'Colombia'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+
+-- 5.
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Where location = 'Colombia'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+-- 6. With the help of a rolling count, checking the vaccination process in Colombia
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.Date, dea.location) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.location = 'Colombia'
+order by 2,3
+
+
+
+
+
